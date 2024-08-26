ecf152b011cb4e5ac9a3c7980fba34337581e83d
Jon Le Marquand <jon.lemarquand@gmail.com>
2024-08-26 22:03:59 +0100

    Add homescreen

--- app/src/main/java/com/jonlemarquand/pokedex/presentation/HomeScreen.kt
+++ app/src/main/java/com/jonlemarquand/pokedex/presentation/HomeScreen.kt
@@ -1,17 +1,182 @@
 package com.jonlemarquand.pokedex.presentation
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.defaultMinSize
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.SearchBar
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithCache
+import androidx.compose.ui.draw.paint
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.asComposePath
+import androidx.compose.ui.graphics.drawscope.drawIntoCanvas
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.graphics.shapes.CornerRounding
+import androidx.graphics.shapes.RoundedPolygon
+import androidx.graphics.shapes.toPath
+import com.jonlemarquand.pokedex.R
+import com.jonlemarquand.pokedex.presentation.components.ButtonWithIcon
+import com.jonlemarquand.pokedex.presentation.ui.theme.Blue
+import com.jonlemarquand.pokedex.presentation.ui.theme.Green
+import com.jonlemarquand.pokedex.presentation.ui.theme.Orange
 import com.jonlemarquand.pokedex.presentation.ui.theme.PokedexTheme
+import com.jonlemarquand.pokedex.presentation.ui.theme.Red
+import com.jonlemarquand.pokedex.presentation.ui.theme.RedFaded
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello!",
-        modifier = modifier
-    )
+    Column(
+        Modifier
+            .fillMaxSize()
+            .background(color = Color.White)
+    ) {
+        Row(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(40.dp)
+                .background(Red)
+                .padding(16.dp, 0.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.pokeball),
+                contentDescription = "Logo",
+                tint = Color.White,
+                modifier = Modifier.size(32.dp)
+                    .padding(0.dp, 0.dp, 8.dp, 0.dp)
+            )
+            Text(
+                text = "Pokedex",
+                modifier = modifier,
+                color = Color.White
+            )
+        }
+        Box(
+            modifier = Modifier
+                .height(350.dp)
+                .fillMaxWidth()
+                .background(color = Red),
+            contentAlignment = Alignment.Center
+        ) {
+            Icon(painter = painterResource(
+                id = R.drawable.pokeball),
+                contentDescription = "Pokeball",
+                tint = RedFaded,
+                modifier = modifier.fillMaxSize()
+            )
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(24.dp)
+            ) {
+                Text(
+                    text = "Find your favourite pokemon",
+                    color = Color.White,
+                    fontSize = 40.sp,
+                    lineHeight = 40.sp
+                )
+                Spacer(modifier = modifier.size(24.dp))
+                SearchBar(
+                    placeholder = {"Search"},
+                    query = "" ,
+                    onQueryChange = { /*TODO*/ } ,
+                    onSearch = { /*TODO*/ } ,
+                    active = false ,
+                    onActiveChange = { /*TODO*/ }
+                ) {
+                    
+                }
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(60.dp)
+                .background(color = Red)
+                .drawWithCache {
+                    // Cache the path for drawing the semicircle
+                    val semicirclePath = androidx.compose.ui.graphics
+                        .Path()
+                        .apply {
+                            arcTo(
+                                rect = androidx.compose.ui.geometry.Rect(
+                                    0f,
+                                    0f,
+                                    size.width,
+                                    size.height * 2
+                                ),
+                                startAngleDegrees = 180f,
+                                sweepAngleDegrees = 180f,
+                                forceMoveTo = false
+                            )
+                            lineTo(size.width, size.height)
+                            lineTo(0f, size.height)
+                            close()
+                        }
+
+                    // Use the cached path for drawing
+                    onDrawWithContent {
+                        drawIntoCanvas { canvas ->
+                            canvas.drawPath(
+                                semicirclePath,
+                                androidx.compose.ui.graphics
+                                    .Paint()
+                                    .apply {
+                                        color = Color.White
+                                    })
+                        }
+                    }
+                }
+        )
+        Column(
+            modifier = modifier.padding(24.dp, 16.dp)
+        ) {
+            ButtonWithIcon(
+                buttonText = "Types",
+                buttonColor = Green,
+                buttonIcon = R.drawable.pokeball
+            )
+            Spacer(modifier = modifier.size(16.dp))
+            ButtonWithIcon(
+                buttonText = "Locations",
+                buttonColor = Orange,
+                buttonIcon = R.drawable.location
+            )
+            Spacer(modifier = modifier.size(16.dp))
+
+            ButtonWithIcon(
+                buttonText = "Moves and Abilities",
+                buttonColor = Blue,
+                buttonIcon = R.drawable.star
+            )
+            Spacer(modifier = modifier.size(16.dp))
+            ButtonWithIcon(
+                buttonText = "Favourites",
+                buttonColor = Red,
+                buttonIcon = R.drawable.heart
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/jonlemarquand/pokedex/presentation/components/ButtonWithBackground.kt
+++ app/src/main/java/com/jonlemarquand/pokedex/presentation/components/ButtonWithBackground.kt
@@ -1,9 +1,12 @@
 package com.jonlemarquand.pokedex.presentation.components
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -12,29 +15,34 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.paint
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.jonlemarquand.pokedex.R
+import com.jonlemarquand.pokedex.presentation.ui.theme.Green
+import com.jonlemarquand.pokedex.presentation.ui.theme.GreenFaded
 import com.jonlemarquand.pokedex.presentation.ui.theme.PokedexTheme
 import com.jonlemarquand.pokedex.presentation.ui.theme.Red
 
 @Composable
-fun ButtonWithIcon(
+fun ButtonWithBackground(
     modifier: Modifier = Modifier,
     buttonText: String,
     buttonColor: Color,
-    buttonIcon: Int
+    buttonIcon: Int,
+    buttonIconColor: Color,
 ) {
     Button(
         onClick = { /*TODO*/ },
-        colors = ButtonDefaults.buttonColors(containerColor = buttonColor)
+        colors = ButtonDefaults.buttonColors(containerColor = buttonColor),
+        modifier = Modifier.height(48.dp)
     ) {
+
         Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(0.dp, 4.dp),
+            modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.SpaceBetween,
             verticalAlignment = Alignment.CenterVertically
         ) {
@@ -45,7 +53,8 @@ fun ButtonWithIcon(
             Icon(
                 painter = painterResource(id = buttonIcon),
                 contentDescription = "Heart icon",
-                modifier = Modifier.size(28.dp)
+                modifier = Modifier.height(102.dp),
+                tint = buttonIconColor
             )
         }
     }
@@ -53,12 +62,13 @@ fun ButtonWithIcon(
 
 @Preview(showBackground = true)
 @Composable
-fun ButtonWithIconPreview() {
+fun ButtonWithBackgroundPreview() {
     PokedexTheme {
-        ButtonWithIcon(
-            buttonText = "Locations",
-            buttonColor = Red,
-            buttonIcon = R.drawable.pokeball
+        ButtonWithBackground(
+            buttonText = "Grass",
+            buttonColor = Green,
+            buttonIcon = R.drawable.grass,
+            buttonIconColor = GreenFaded
         )
     }
 }
